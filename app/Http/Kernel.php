96cbdbd30fd4d0b087f927ccbe23cf006ea4ba30
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-08-03 00:56:42 +0900

    Email認証 ブログの通りにやったつもりだが下記の動きが出ない。 ・Email認証する前にもログインできてしまう ・Email認証メールが送られてから一定時間が経つと承認できなくなるが、その時間の設定がわからない ・/Session::flushで描かれているメッセージが表示されない。
    これから実装する機能
    ・Email再送信

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,5 +49,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'confirm' => \App\Http\Middleware\VerifyConfirmed::class, // ①Email認証時追加
     ];
 }
--- app/Http/Middleware/VerifyConfirmed.php
+++ app/Http/Middleware/VerifyConfirmed.php
@@ -3,18 +3,19 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\User;
 
-class VerifyConfirmed
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
+class VerifyConfirmed{
     public function handle($request, Closure $next)
     {
+      $user = User::where('email', '=', $request->input('email'))->first();
+        if ($user) {
+            if(! $user->isConfirmed()) {
+                \Session::flash('flash_message', 'ユーザー登録確認メールに従って、ユーザーを有効化してください。');
+                return redirect()->back()->withInput($request->only('email'));
+            }
+        }
+        
         return $next($request);
     }
 }
