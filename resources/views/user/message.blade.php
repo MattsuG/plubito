2d8a8967f49407f1b6576d9990e33ffe56d690d4
今陸成 <rikutech@gmail.com>
2016-08-24 12:38:22 +0900

    message.blade.php修正

--- resources/views/user/message.blade.php
+++ resources/views/user/message.blade.php
@@ -2,11 +2,12 @@
 @extends('common.layout')
 @section('TitleAndCss')
 <title>mypage.php | マイページ</title>
-<link rel="stylesheet" href="{{{asset('/assets/bootstrap/css/bootstrap.min.css')}}}">
-<link rel="stylesheet" href="{{{asset('/assets/font-awesome/css/font-awesome.min.css')}}}">
-<link rel="stylesheet" href="{{{asset('/assets/css/style_pre_index.css')}}}">
-<link rel="stylesheet" href="{{{asset('/assets/css/animate.css')}}}">
-<link rel="stylesheet" href="{{{asset('/assets/css/style.css')}}}">
+
+<link rel="stylesheet" href="{{{secure_asset('/assets/bootstrap/css/bootstrap.min.css')}}}">
+<link rel="stylesheet" href="{{{secure_asset('/assets/font-awesome/css/font-awesome.min.css')}}}">
+<link rel="stylesheet" href="{{{secure_asset('/assets/css/style_pre_index.css')}}}">
+<link rel="stylesheet" href="{{{secure_asset('/assets/css/animate.css')}}}">
+<link rel="stylesheet" href="{{{secure_asset('/assets/css/style.css')}}}">
 <style>
     p{
       width: 100px;
@@ -16,7 +17,6 @@
       -o-text-overflow: ellipsis; /* Opera9,10対応 */
     }
 </style>
-
 <!-- <link href="css/style_pre_index.css" rel="stylesheet"> -->
 @stop
 
@@ -27,45 +27,39 @@
             <ul class="nav metismenu" id="side-menu">
                 <li class="nav-header">
                     <div class="dropdown profile-element">                         
-                            <a href="/mentor"><img alt="image" class="img" src="{{{asset('/assets/img/plus.png')}}}" width="25px" height="50px" /></a>
+                            <a href="/mentor"><img alt="image" class="img" src="{{{secure_asset('/assets/img/plus.png')}}}" width="25px" height="50px" /></a>
                     </div>
                     <div class="dropdown profile-element"> 
                         <span>
-                            @if (!empty(Auth::user()->profile_picture_path))
-                            <img alt="image" class="img-circle" src="{{{asset(Auth::user()->profile_picture_path)}}}">
-                            @endif
-                            @if (empty(Auth::user()->profile_picture_path))
-                            <img alt="image" class="img-circle" src="/assets/img/default_thumbnail.jpg">
-                            @endif
+                            <img alt="image" class="img-circle" src="{{{secure_asset('/assets/img/about/1.jpg')}}}" />
                         </span>
                         <a class="dropdown-toggle" href="#">
                             <span class="clear"> 
                                 <span class="block m-t-xs"> 
-                                    <strong class="font-bold">{{ Auth::user()->name }}</strong>
+                                    <strong class="font-bold">松澤隼人</strong>
                                 </span> 
-                                <span class="text-muted text-xs block">{{ Auth::user()->introduction }}</span>
+                                <span class="text-muted text-xs block">スポーツメーカーを退職後、大阪にて教育系ウェブサービスの立ち上げ準備中。2016年4月から6月までセブ島留学をしてプログラミングを学び、その時のルームメートと個人間で進路情報をシェアできるサービスを構築中。</span>
                             </span>
                         </a>                        
                     </div>                    
                     <div class="logo-element">
-                        <a href="/mentor"><img alt="image" class="img" src="{{{asset('/assets/img/plus.png')}}}" width="25px" height="50px" /></a>
+                        <a href="/"><img alt="image" class="img" src="{{{secure_asset('/assets/img/plus.png')}}}" width="25px" height="50px" /></a>
                     </div>
                 </li>
                 <li>
-                    <a href="/user/show/{{ Auth::user()->id }}"><i class="fa fa-user"></i> <span class="nav-label">プロフィール表示</span></a>
+                    <a href="#"><i class="fa fa-pencil"></i> <span class="nav-label">アカウント編集</span><span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="/user/edit">プロフィール編集</a></li>
+                        <li><a href="graph_morris.html">Email編集</a></li>
+                        <li><a href="graph_rickshaw.html">パスワード編集</a></li>
+                    </ul>
                 </li>
                 <li>
-                    <a href="/user/edit/{{ Auth::user()->id }}"><i class="fa fa-pencil"></i> <span class="nav-label">プロフィール編集</span></a>
+                    <a href="add"><i class="fa fa-bullhorn"></i> <span class="nav-label">トーク作成</span></a>
                 </li>
-                <li>
-                    <a href="{{ url('mentor/create')}}"><i class="fa fa-bullhorn"></i> <span class="nav-label">トーク作成</span></a>
-                </li>
-                 <li>
-                    <a href="/user/mypage"><i class="fa fa-comment"></i> <span class="nav-label">メッセージ</span></a>
-                </li>  
                 <li>
                     <a href="/user/mypage"><i class="fa fa-calendar"></i> <span class="nav-label">マイページ</span></a>
-                </li>                 
+                </li>               
             </ul>
         </div>
     </nav>
@@ -212,7 +206,7 @@
                         <div class="panel-heading">
                             <div class="panel-options">
                                 <ul class="nav nav-tabs">
-                                    <li class="active"><a href="#tab-1" data-toggle="tab">受信</a></li>
+                                    <li class=""><a href="#tab-1" data-toggle="tab">受信</a></li>
                                     <li class=""><a href="#tab-2" data-toggle="tab">送信</a></li>
                                 </ul>
                             </div>
@@ -236,7 +230,7 @@
                                 @foreach($received_mails as $received_mail)
                                     <tr>
                                         <td>
-                                            <img alt="profile_image" class="img-circle" src="{{ asset($received_mail->sender->profile_picture_path) }}">
+                                            <img alt="profile_image" class="img-circle" src="{{ secure_asset($received_mail->sender->profile_picture_path) }}">
                                         </td>
                                         <td>
                                             {{ $received_mail->sender->name }}
@@ -272,7 +266,7 @@
                                 @foreach ($sent_mails as $sent_mail)
                                 <tr>
                                     <td>
-                                        <img alt="profile_image" class="img-circle" src="{{ asset($sent_mail->receiver->profile_picture_path) }}">
+                                        <img alt="profile_image" class="img-circle" src="{{ secure_asset($sent_mail->receiver->profile_picture_path) }}">
                                     </td>
                                     <td>
                                         {{ $sent_mail->receiver->name }}
@@ -287,35 +281,6 @@
                                         <a href="{{ url('/user/message/'.$sent_mail->talk_id) }}">詳細を見る</a>
                                     </td>
                                 </tr>
-                                <div class="modal" id="request-modal" tabindex="-1">
-                                    <div class="modal-dialog">
-                                    <!-- 3.モーダルのコンテンツ -->
-                                        <div class="modal-content">
-                                            <!-- 4.モーダルのヘッダ -->
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                                <h4 class="modal-title" id="modal-label">日時を入力してリクエストを承認してください。</h4>
-                                            </div>
-                                            <!-- 5.モーダルのボディ -->
-                                            <div class="modal-body">
-                                                <p>{{  }}</p>
-                                                <p>{{  }}</p>
-                                                <!-- <input type=""> -->
-                                            </div>
-                                            <!-- 6.モーダルのフッタ -->
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">やめる</button>
-                                                <form method="post" action="{{ url('/mentor/apply') }}">
-                                                    <input name="talk_id" value="{{ $talk->id }}" type="hidden">
-                                                    {{ csrf_field() }}
-                                                    <input type="submit" class="btn btn-primary" value="申し込む">
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                                 @endforeach
                                 </tbody>
                             </table>
@@ -340,12 +305,12 @@
 @stop
 
 @section('Js')
-<script src="{{{asset('/assets/js/jquery-2.1.1.js')}}}"></script>
-<script src="{{{asset('/assets/bootstrap/js/bootstrap.min.js')}}}"></script>
+<script src="{{{secure_asset('/assets/js/jquery-2.1.1.js')}}}"></script>
+<script src="{{{secure_asset('/assets/bootstrap/js/bootstrap.min.js')}}}"></script>
 
-<script src="{{{asset('/assets/js/plugins/metisMenu/jquery.metisMenu.js')}}}"></script>
-<script src="{{{asset('/assets/js/plugins/slimscroll/jquery.slimscroll.min.js')}}}"></script>
+<script src="{{{secure_asset('/assets/js/plugins/metisMenu/jquery.metisMenu.js')}}}"></script>
+<script src="{{{secure_asset('/assets/js/plugins/slimscroll/jquery.slimscroll.min.js')}}}"></script>
 
-<script src="{{{asset('/assets/js/inspinia.js')}}}"></script>
-<script src="{{{asset('/assets/js/plugins/pace/pace.min.js')}}}"></script>
+<script src="{{{secure_asset('/assets/js/inspinia.js')}}}"></script>
+<script src="{{{secure_asset('/assets/js/plugins/pace/pace.min.js')}}}"></script>
 @stop
