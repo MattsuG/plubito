5d69b416d2126d6e2265b20836092b2499e48fc9
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-09-02 16:40:36 +0900

    微調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="/">Plubito Landing Page</a>
+				<a class="navbar-brand" href="/"></a>
 			</div>
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="top-navbar-1" style="background-color: #fff">
