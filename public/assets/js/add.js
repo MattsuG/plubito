e73557e098dcb0d8fb8eb4c7130b511e34b07764
今陸成 <rikutech@gmail.com>
2016-09-16 14:08:23 +0900

    Revert "文字数カウンターの判定文字数を修正"
    This reverts commit 93fefaeac21ef1c587efc9abb38521016b6be335.

--- public/assets/js/add.js
+++ public/assets/js/add.js
@@ -52,7 +52,7 @@
     $('#detail').bind('keydown keyup keypress change',function(){
         var thisValueLength = $(this).val().length;
         $('#detail_count').text(thisValueLength);
-        if(thisValueLength > 1000) {
+        if(thisValueLength > 500) {
             $('#detail_count').addClass('cebroad-pink');
         } else {
             $('#detail_count').removeClass('cebroad-pink');
